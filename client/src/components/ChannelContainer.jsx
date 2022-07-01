c9336d50ad4a39d9e9f359e212d2178fa01abf86
ManuelP2 <manuelperezp22@hotmail.com>
2022-07-01 12:04:40 -0400

    channel container custom components

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Channel, useChatContext, MessageTeam } from 'stream-chat-react';
 
-function ChannelContainer() {
+import { ChannelInner, CreateChannel, EditChannel } from './'
+
+const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) => {
+
+  const { channel } = useChatContext();
+
+  if(isCreating) {
+    return (
+        <div className="channel__container">
+            <CreateChannel createType={createType} setIsCreating={setIsCreating} />
+        </div>
+    )
+  }
+
+  if(isEditing) {
+    return (
+      <div className="channel__container">
+          <EditChannel setIsEditing={setIsEditing} />
+      </div>
+    )
+  }
+
+  const EmptyState = () => (
+    <div className="channel-empty__container">
+        <p className="channel-empty__first">Comienzo de historial de chat.</p>
+        <p className="channel-empty__second">Envia mensajes, links, emojis y mas!</p>
+    </div>
+  )
+
   return (
-    <div>ChannelContainer</div>
+    <div className="channel__container">
+      <Channel EmptyStateIndicator={EmptyState} Message={(messageProps, i) => <MessageTeam key={i} { ...messageProps }/>}>
+          <ChannelInner setIsEditing={setIsEditing} />
+      </Channel>
+    </div>
   )
 }
 
